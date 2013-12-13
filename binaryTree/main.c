0bcb16851ea68ceb7fbf692177a0af3dd2d4136f
nianyuguai <840311035@qq.com>
2013-12-14 00:12:48 +0800

    完成二叉树的插入、打印

--- binaryTree/main.c
+++ binaryTree/main.c
@@ -12,13 +12,57 @@ NodeTree* makeEmpty(NodeTree* Tree)
 {
     if(Tree != NULL)
     {
-
+        makeEmpty(Tree->left);
+        makeEmpty(Tree->right);
+        free(Tree);
     }
+    return NULL;
+}
+
+NodeTree* insertTree(NodeTree* Tree,int element)
+{
+    if(Tree == NULL)
+    {
+        Tree = malloc(sizeof(NodeTree));
+        Tree->data = element;
+        Tree->left = Tree->right = NULL;
+    }else if(element < Tree->data)
+    {
+        Tree->left = insertTree(Tree->left,element);
+    }else if(element > Tree->data)
+    {
+        Tree->right = insertTree(Tree->right,element);
+    }
+    return Tree;
+}
+
+void printfTree(NodeTree* Tree)
+{
+    if(Tree == NULL)
+        return;
+    else{
+        printfTree(Tree->left);
+        printf("%d ",Tree->data);
+        printfTree(Tree->right);
+    }
+
 }
 
 int main()
 {
-
+    int n,i,nElement;
+    NodeTree* Tree;
+    while(~scanf("%d",&n))
+    {
+        Tree = makeEmpty(NULL);
+        for(i = 0;i < n;i++)
+        {
+            scanf("%d",&nElement);
+            Tree = insertTree(Tree,nElement);
+        }
+        printfTree(Tree);
+        printf("\n");
+    }
     printf("Hello world!\n");
     return 0;
 }
