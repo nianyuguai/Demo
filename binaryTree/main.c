3a7788732cdbe61a4381f9aba3802c87cb45d84f
nianyuguai <840311035@qq.com>
2013-12-14 01:44:25 +0800

    添加二叉树删除节点操作

--- binaryTree/main.c
+++ binaryTree/main.c
@@ -36,6 +36,54 @@ NodeTree* insertTree(NodeTree* Tree,int element)
     return Tree;
 }
 
+NodeTree* findMin(NodeTree* Tree)
+{
+    if(Tree == NULL)
+        return NULL;
+    else if(Tree->left == NULL)
+        return Tree;
+    else
+        return findMin(Tree->left);
+}
+
+NodeTree* deleteTree(NodeTree* Tree,int element)
+{
+    //删除节点有几种情况：左右子节点都存在；
+    //只有左节点；只有右节点；不存在左右节点；找不到元素
+    NodeTree *tmpTree;
+
+    if(Tree == NULL)
+        printf("can't find the element");
+    else if(Tree->data > element)
+    {
+        Tree->left = deleteTree(Tree->left,element);
+    }
+    else if(Tree->data < element)
+    {
+         Tree->right = deleteTree(Tree->right,element);
+    }
+    else if(Tree->left && Tree->right)
+    {
+        //存在2个子节点，左边的指针不用修改
+        tmpTree = findMin(Tree->right);//找到右边最小的节点
+        Tree->data = tmpTree->data;//将最小的节点放到删除的节点处
+        Tree->right = deleteTree(Tree->right,Tree->data);//修改右边的指针
+    }else
+    {
+        //将要删除的节点保存
+        tmpTree = Tree;
+        if(Tree->left == NULL)
+        {
+            Tree = Tree->right;  //指针不用修改，直接将右边节点替换删除节点
+        }else if(Tree->right == NULL)
+        {
+            Tree = Tree->left;
+        }
+        free(tmpTree);//释放删除节点占用的空间
+    }
+    return Tree;
+}
+
 void printfTree(NodeTree* Tree)
 {
     if(Tree == NULL)
@@ -45,7 +93,6 @@ void printfTree(NodeTree* Tree)
         printf("%d ",Tree->data);
         printfTree(Tree->right);
     }
-
 }
 
 int main()
@@ -60,6 +107,11 @@ int main()
             scanf("%d",&nElement);
             Tree = insertTree(Tree,nElement);
         }
+        printfTree(Tree);
+        printf("\n");
+        printf("delete number:");
+        scanf("%d",&nElement);
+        Tree = deleteTree(Tree,nElement);
         printfTree(Tree);
         printf("\n");
     }
