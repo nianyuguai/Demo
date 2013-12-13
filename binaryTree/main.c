58bd740e621c35a3a93c7e2889ac472b93c2bcf8
nianyuguai <840311035@qq.com>
2013-12-13 23:32:33 +0800

    binaryTree
    建立结构体

---
+++ binaryTree/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+}NodeTree;
+
+NodeTree* makeEmpty(NodeTree* Tree)
+{
+    if(Tree != NULL)
+    {
+
+    }
+}
+
+int main()
+{
+
+    printf("Hello world!\n");
+    return 0;
+}
